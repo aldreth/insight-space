d01ce5743434a564b84a8c97203609d4cecd5f5b
Edward Andrews-Hodgson <edward@aldreth.com>
2025-06-13 19:40:09 +0100

    Initial personalisation

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,24 +1,23 @@
 export const url = process.env.URL || 'http://localhost:8080';
 // Extract domain from `url`
 export const domain = new URL(url).hostname;
-export const siteName = 'Eleventy Excellent';
-export const siteDescription = 'Eleventy starter for building modern, resilient websites';
+export const siteName = 'Insight Space';
+export const siteDescription =
+  'Leadership and Team insight Coaching, for those looking to contribute to the future of our world.  Tapping into inherent knowing, insight and wisdom.  Coaching for a time between worlds.';
 export const siteType = 'Person'; // schema
 export const locale = 'en_EN';
 export const lang = 'en';
 export const skipContent = 'Skip to content';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
-  avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
+  name: 'Fay Andrews-Hodgson', // i.e. Lene Saile - page / blog author's name. Must be set.
+  avatar: '/fay.jps', // path to the author's avatar. In this case just using a favicon.
+  email: 'fay@insightspace.co.uk', // i.e. fay@insightspace.co.uk - email of the author
+  website: 'https://insightspace.co.uk' // i.e. https.://www.lenesaile.com - the personal site of the author
 };
 export const creator = {
-  name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
-  email: 'hola@lenesaile.com',
-  website: 'https://www.lenesaile.com',
-  social: 'https://front-end.social/@lene'
+  name: 'Fay Andrews-Hodgson', // i.e. Lene Saile - creator's (developer) name.
+  email: 'fay@insightspace.co.uk',
+  website: 'https://insightspace.co.uk'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
 export const themeColor = '#dd4462'; // used in manifest, for example primary color value
@@ -26,7 +25,7 @@ export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light 
 export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
 export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
 export const opengraph_default_alt =
-  "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
+  'Leadership and Team insight Coaching, for those looking to contribute to the future of our world.  Tapping into inherent knowing, insight and wisdom.  Coaching for a time between worlds.'; // alt text for default meta image"
 export const blog = {
   // RSS feed
   name: 'My Web Development Blog',
@@ -91,4 +90,4 @@ export const viewRepo = {
   allow: true,
   infoText: 'View this page on GitHub'
 };
-export const easteregg = true;
+export const easteregg = false;
